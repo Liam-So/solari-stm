dff76cc24461be0a114918a329184a42ae7e3402
Liam-So <Liam.So@dal.ca>
2025-02-19 15:52:45 -0500

    playing around with animations

---
+++ src/app/components/Flap.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Flap = ({ bottom, animated, final, hinge, children }) => {
+  const classes = `flap 
+  ${bottom ? bottom : top} 
+  ${animated ? animated : ""} 
+  ${final ? final : ""}`.trim();
+
+  return (
+    <div className={classes}>
+      {children}
+      {hinge && <div className="hinge" data-kind='hinge' />}
+    </div>
+  )
+}
+
+Flap.defaultProps = {
+  bottom: false,
+  animated: false,
+  final: false,
+  hinge: false
+}
+
+Flap.propTypes = {
+  bottom: PropTypes.bool,
+  animated: PropTypes.bool,
+  final: PropTypes.bool,
+  hinge: PropTypes.bool
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,44 @@
 
 import React, { useState, useEffect } from 'react';
 import FlipRow from './components/FlipRow';
+import FlapDisplay from './components/FlapDisplay';
+import {Presets} from './components/Presets';
 import { fetchMTAData } from './services/mtaServices';
 
-/*
-TODO:
-1. Import the static GTFS data into a folder
-2. Create a script that will get the stop ids for grand central station in stops.txt
-3. Create a lookup table for the stops.txt. Make the key the ID and the value the destination name.
-    - Should we store it in a file somewhere?
-4. Get the last stop in tripUpdate.stopTimeUpdate. This will be our destination.
-
-*/
 type Train = {
   line: string;
   destination: string;
   time: string;
 }
 
+const Words = [
+  'Washington',
+  'Baltimore',
+  'Philadelphia',
+  'Newark',
+  'New York',
+  'New Haven',
+  'Providence',
+  'Boston'
+]
+
+const Modes = {
+  Numeric: 0,
+  Alphanumeric: 1,
+  Words: 2
+}
+
 const SolariBoard = () => {
   const [boardData, setBoardData] = useState<Train[]>([]);
+  const [mode, setMode] = useState(Modes.Alphanumeric)
+  const [chars, setChars] = useState(Presets.ALPHANUM)
+  const [words, setWords] = useState(Words)
+  const [length, setLength] = useState(40)
+  const [timing, setTiming] = useState(30)
+  const [padChar, setPadChar] = useState(' ')
+  const [padMode, setPadMode] = useState('auto')
+  const [value, setValue] = useState('hello')
+  const [hinge, setHinge] = useState(true)
 
   useEffect(() => {
     const updateBoard = async () => {
@@ -52,7 +71,21 @@ const SolariBoard = () => {
           <div className="flex-1 pl-4">DESTINATION</div>
           <div className="text-center pr-4 md:pr-20">TIME</div>
         </div>
-        {boardData.map((row, index) => (
+
+        <button className='p-1 rounded-lg text-xs bg-red-50' onClick={() => setValue("TORONTO")}>Click me</button>
+
+        <FlapDisplay 
+          value={value}
+          chars={chars}
+          words={mode === Modes.Words ? words : undefined}
+          length={length}
+          timing={timing}
+          hinge={hinge}
+          padChar={padChar}
+          padMode={padMode}
+        />
+
+        {/* {boardData.map((row, index) => (
           <FlipRow
             key={index}
             trainLine={row.line}
@@ -60,7 +93,7 @@ const SolariBoard = () => {
             time={row.time}
             onRowComplete={() => {}}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   );
@@ -69,25 +102,25 @@ const SolariBoard = () => {
 export default SolariBoard;
 
 // Add this to your global CSS or style tag
-// const styles = `
-// @keyframes flip {
-//   0% {
-//     transform: rotateX(0deg);
-//     background-color: #000;
-//   }
-//   50% {
-//     transform: rotateX(90deg);
-//     background-color: #111;
-//   }
-//   100% {
-//     transform: rotateX(180deg);
-//     background-color: #000;
-//   }
-// }
+const styles = `
+@keyframes flip {
+  0% {
+    transform: rotateX(0deg);
+    background-color: #000;
+  }
+  50% {
+    transform: rotateX(90deg);
+    background-color: #111;
+  }
+  100% {
+    transform: rotateX(180deg);
+    background-color: #000;
+  }
+}
 
-// .animate-flip {
-//   animation: flip 0.05s linear;
-//   transform-style: preserve-3d;
-//   backface-visibility: hidden;
-// }
-// `;
+.animate-flip {
+  animation: flip 0.05s linear;
+  transform-style: preserve-3d;
+  backface-visibility: hidden;
+}
+`;
